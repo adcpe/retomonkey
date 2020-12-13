9daba7d22aec3bd40b27a181f3dd8536c48ba68e
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-13 17:17:05 -0500

    Changed model fieds to CharField

--- places/models.py
+++ places/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 
 class Place(models.Model):
-    place_id = models.TextField()
-    name = models.TextField()
-    address = models.TextField()
-    maps_url = models.TextField()
+    place_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    maps_url = models.CharField(max_length=200)
 
-# Model         Google Maps API
-# --------------------------------
-# place_id      place_id
-# name          name
-# address       formatted_address
-# maps_url      url
+
+"""
+Model       |   Google Maps API
+------------|---------------------
+place_id    |   place_id
+name        |   name
+address     |   formatted_address
+maps_url    |   url
+"""
