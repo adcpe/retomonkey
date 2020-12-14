0ca94ed6b6798e87d2cad534e8e8a038e528dccd
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-14 12:04:06 -0500

    Extra changes for Heroku

--- retomonkey/settings.py
+++ retomonkey/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 import django_heroku
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,12 +124,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    [BASE_DIR / 'static']
+    os.path.join(BASE_DIR, 'static'),
 )
 
 django_heroku.settings(locals())
