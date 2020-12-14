5c0ec528f9c75cb43a696cbb52f01484bba97f83
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-14 10:59:35 -0500

    Added some fixes to the form

--- places/views.py
+++ places/views.py
@@ -5,9 +5,11 @@ from .helpers import get_places, post_places
 
 def index(request):
     place = request.POST.get("place")
-    places = get_places(place)
 
-    for place in places:
-        post_places(place)
+    if place:
+        places_ids = get_places(place)
+
+        for ids in places_ids:
+            post_places(ids)
 
     return render(request, "index.html", {})
